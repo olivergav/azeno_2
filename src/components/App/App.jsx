026e32da07003a3a72b0cb63300841aa29d855d2
olivergav <oliwier.gawlik0@gmail.com>
2022-05-21 16:14:44 +0200

    Added CookieWrapper for showing different paths

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,24 +1,18 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import './App.scss';
 import Header from '../Header/Header';
-import Train from '../Train/Train';
-import Profile from '../Profile/Profile';
-import Login from '../Auth/Login';
-import Register from '../Auth/Register';
+import useCookie from '../hooks/useCookie';
+import CookieWrapper from '../CookieWrapper/CookieWrapper';
 
 function App() {
+    const [cookie, setCookie] = useCookie('token_auth');
+
     return (
         <BrowserRouter>
             <Header />
-            <Routes>
-                <Route path="/" element={<Link to="/profile">Profile</Link>} />
-                <Route path="/profile" element={<Profile />} />
-                <Route path="/train" element={<Train />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-            </Routes>
+            <CookieWrapper cookie={cookie} setCookie={setCookie} />
         </BrowserRouter>
     );
 }
---
+++ src/components/hooks/useCookie.js
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react';
+
+const useCookie = (cookieName) => {
+    const [cookie, setCookie] = useState(undefined);
+
+    useEffect(() => {
+        const val = document.cookie
+            .split('; ')
+            .filter((item) => item.startsWith(`${cookieName}=`))[0];
+        console.log(val);
+
+        if (val !== undefined) {
+            setCookie(val.split('=')[1]);
+        }
+    }, [cookieName]);
+
+    useEffect(() => {
+        if (cookie !== undefined) {
+            document.cookie = `${cookieName}=${cookie}`;
+        }
+    }, [cookie]);
+
+    return [cookie, setCookie];
+};
+
+export default useCookie;
