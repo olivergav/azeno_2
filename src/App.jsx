3cc6a10280579cb28cfb86db01953aacd4a9513d
olivergav <oliwier.gawlik0@gmail.com>
2022-05-21 11:36:21 +0200

    add support for boostrap and boostrap-react

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.scss';
 
+import Button from 'react-bootstrap/Button';
+
 function App() {
     return (
         <div>
             <h1>Azeno</h1>
+            <Button variant="primary">Button #1</Button>
         </div>
     );
 }
