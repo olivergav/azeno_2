16df0fd39dd968cc1e6eadfbddae19a4f561df99
olivergav <oliwier.gawlik0@gmail.com>
2022-05-22 09:37:58 +0200

    revert homework changes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 
 import './App.scss';
 import Header from '../Header/Header';
 import useCookie from '../hooks/useCookie';
-import CookieWrapper from '../CookieWrapper/CookieWrapper';
+import Profile from '../Profile/Profile';
+import Train from '../Train/Train';
+import Login from '../Auth/Login';
+import Register from '../Auth/Register';
 
 function App() {
     const [cookie, setCookie] = useCookie('token_auth');
@@ -12,7 +15,13 @@ function App() {
     return (
         <BrowserRouter>
             <Header />
-            <CookieWrapper cookie={cookie} />
+            <Routes>
+                <Route path="/" element={<Link to="/profile">Profile</Link>} />
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/train" element={<Train />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+            </Routes>
         </BrowserRouter>
     );
 }
