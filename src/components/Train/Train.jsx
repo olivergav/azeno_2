70ea514c4b4cb13904aa20d025792c8ae6062b3a
olivergav <oliwier.gawlik0@gmail.com>
2022-05-22 11:25:23 +0200

    get data from backend api and render it using train component

--- src/components/Train/Train.jsx
+++ src/components/Train/Train.jsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Card from '../Card/Card';
 
 function Train() {
-    return <div>Train</div>;
+    const [cards, setCards] = useState([]);
+
+    useEffect(() => {
+        getSets().then((data) => setCards(data));
+    }, []);
+
+    async function getSets() {
+        const url = 'http://localhost:3010';
+        const endpoint = 'sets';
+        const response = await fetch(`${url}/${endpoint}`);
+
+        return await response.json();
+    }
+
+    return (
+        <div>
+            {cards.map(({ id, name, level, quantity, image }) => {
+                <Card key={id} name={name} level={level} quantity={quantity} image={image} />;
+            })}
+        </div>
+    );
 }
 
 export default Train;
